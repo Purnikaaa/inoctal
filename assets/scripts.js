5021b25d3735113a41cbbd09931982f5243c07a5
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2023-10-16 00:08:12 +0100

    Added event listener for navegation button and style archive

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,8 +1,10 @@
 //JavaScript of responsive navigation menu
 
 const menuBtn = document.querySelector(".menu-btn");
+const navegation = document.querySelector(".navegation");
 
 menuBtn.addEventListener("click", () => {
   //si la clase "active" está presente la elimina, de lo contrario la añade
   menuBtn.classList.toggle("active");
+  navegation.classList.toggle("active");
 });
