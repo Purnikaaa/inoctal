321b4b44f86d050fc2c862bf455033b554515679
Evelin Alvarado <129849677+EvelinAlvarado@users.noreply.github.com>
2023-10-16 19:50:47 +0100

    Implemented navigation functionality for the buttons in JavaScript

--- assets/scripts.js
+++ assets/scripts.js
@@ -8,3 +8,38 @@ menuBtn.addEventListener("click", () => {
   menuBtn.classList.toggle("active");
   navegation.classList.toggle("active");
 });
+
+//JavaScript for video slider navegation
+const btns = document.querySelectorAll(".nav-btn");
+const slides = document.querySelectorAll(".video-slide");
+const contents = document.querySelectorAll(".content");
+
+var sliderNav = function (manual) {
+  //button
+  btns.forEach((btn) => {
+    btn.classList.remove("active");
+  });
+
+  //video
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+
+  //content
+  contents.forEach((content) => {
+    content.classList.remove("active");
+  });
+
+  //button
+  btns[manual].classList.add("active");
+  //video
+  slides[manual].classList.add("active");
+  //content
+  contents[manual].classList.add("active");
+};
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    sliderNav(i);
+  });
+});
